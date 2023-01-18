97048e848124156df1db08d518ce0f3032731811
danilkorepanov <97517510+danilkorepanov@users.noreply.github.com>
2023-01-18 03:57:30 +0000

    data

--- src/components/main/Content/components/ContentItems/ContentItems.jsx
+++ src/components/main/Content/components/ContentItems/ContentItems.jsx
@@ -1,11 +1,22 @@
 import ContentItem from "./ContentItem";
-import dataPizzas from "../../../../../assets/pizzas.json";
+import React from "react";
 
 const ContentItems = () => {
+
+  // get pizza data
+  const [items, setItems ] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("https://63c7634c5c0760f69ab6be90.mockapi.io/items").then ( res => {return res.json()}
+  ).then(arr => {setItems(arr)})
+  }, [])
+ 
+   // get pizza data
+
   return (
     <div className="content__items">
-      {dataPizzas.map((pizza) => (
-        <ContentItem dataPizza={pizza} key={pizza.id} />
+      {items.map((item) => (
+        <ContentItem dataPizza={item} key={item.id} />
       ))}
     </div>
   );
