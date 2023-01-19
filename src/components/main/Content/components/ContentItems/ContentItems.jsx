4faee0b60b155d599ce8fcf28384c8bb95c5e84f
danilkorepanov <97517510+danilkorepanov@users.noreply.github.com>
2023-01-19 03:51:52 +0000

    Skeleton

--- src/components/main/Content/components/ContentItems/ContentItems.jsx
+++ src/components/main/Content/components/ContentItems/ContentItems.jsx
@@ -1,21 +1,29 @@
 import ContentItem from "./ContentItem";
 import React from "react";
+import ContentItemSkeleton from "./Components/ContentItemSkeleton";
+
 
 const ContentItems = () => {
+
+  const [isLoading, setIsLoading] = React.useState(true)
 
   // get pizza data
   const [items, setItems ] = React.useState([])
 
   React.useEffect(() => {
     fetch("https://63c7634c5c0760f69ab6be90.mockapi.io/items").then ( res => {return res.json()}
-  ).then(arr => {setItems(arr)})
+  ).then(arr => {
+    setItems(arr)
+    setIsLoading(false)
+  })
   }, [])
  
    // get pizza data
 
   return (
     <div className="content__items">
-      {items.map((item) => (
+      { isLoading ? [...new Array(6)].map((_, i) => <ContentItemSkeleton key={i}/> ) :
+      items.map((item) => (
         <ContentItem dataPizza={item} key={item.id} />
       ))}
     </div>
